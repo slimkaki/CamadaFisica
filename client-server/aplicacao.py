3a397a0e71fc22403f37884e8ca6d6140bb98199
slimkaki <rafaalmada98@gmail.com>
2019-08-16 00:42:00 -0300

    aplicacao via terminal pronta

--- client-server/aplicacao.py
+++ client-server/aplicacao.py
@@ -8,11 +8,52 @@
 #  Aplicação 
 ####################################################
 
-print("comecou")
-
+import subprocess
 from enlace import *
 import time
+from server import *
 
+print("\n")
+print("------------------------------------")
+print("   Sistema Client/Server feito por  ")
+print("     Lucas Leal e Rafael Almada     ")
+print("Camada Física da Computação - Insper")
+print("      Engenharia da Computação      ")
+print("------------------------------------")
+print("\n")
+
+getSerial = subprocess.check_output("python -m serial.tools.list_ports", shell=True)
+getSerial = getSerial.decode("utf-8")
+getSerial = getSerial.split("\n")
+print("Qual porta do seu computador você está utilizando?")
+for i in range(len(getSerial)):
+  print("{} - {}".format(i, getSerial[i]))
+  print("\n")
+print("Digite o número da porta...\n")
+choice = int(input("> "))
+if (choice < len(getSerial) and choice > 0):
+  serialName = getSerial[choice]
+else:
+  print("Digite a porta a seguir:")
+  serialName = input("> ")
+
+print("abriu com")
+
+print("\n")
+print("---------------------------------")
+print("Você deseja ser:")
+print("1 - Server")
+print("2 - Client")
+platform = int(input("> "))
+
+if (platform == 1):
+  serv = Server(serialName)
+  serv.comunicate()
+elif (platform == 2):
+  cli = Client(serialName)
+  cli.comunicate()
+else:
+  print("[ERRO] Digite um número")
 
 # Serial Com Port
 #   para saber a sua porta, execute no terminal :
@@ -20,73 +61,73 @@ import time
 
 #serialName = "/dev/ttyACM0"           # Ubuntu (variacao de)
 #serialName = "/dev/tty.usbmodem1411" # Mac    (variacao de)
-serialName = "COM11"                  # Windows(variacao de)
-print("abriu com")
+#serialName = "COM11"                  # Windows(variacao de)
+#print("abriu com")
 
-def main():
-    # Inicializa enlace ... variavel com possui todos os metodos e propriedades do enlace, que funciona em threading
-    com = enlace(serialName) # repare que o metodo construtor recebe um string (nome)
-    # Ativa comunicacao
-    com.enable()
+# def main():
+#     # Inicializa enlace ... variavel com possui todos os metodos e propriedades do enlace, que funciona em threading
+#     com = enlace(serialName) # repare que o metodo construtor recebe um string (nome)
+#     # Ativa comunicacao
+#     com.enable()
 
    
 
-    # Log
-    print("-------------------------")
-    print("Comunicação inicializada")
-    print("  porta : {}".format(com.fisica.name))
-    print("-------------------------")
+#     # Log
+#     print("-------------------------")
+#     print("Comunicação inicializada")
+#     print("  porta : {}".format(com.fisica.name))
+#     print("-------------------------")
 
-    # Carrega dados
-    print ("gerando dados para transmissao :")
+#     # Carrega dados
+#     print ("gerando dados para transmissao :")
   
-      #no exemplo estamos gerando uma lista de bytes ou dois bytes concatenados
+#       #no exemplo estamos gerando uma lista de bytes ou dois bytes concatenados
     
-    #exemplo 1
-    #ListTxBuffer =list()
-    #for x in range(1,10):
-    #    ListTxBuffer.append(x)
-    #txBuffer = bytes(ListTxBuffer)
+#     #exemplo 1
+#     #ListTxBuffer =list()
+#     #for x in range(1,10):
+#     #    ListTxBuffer.append(x)
+#     #txBuffer = bytes(ListTxBuffer)
     
-    #exemplo2
-    txBuffer = bytes([2]) + bytes([3])+ bytes("teste", 'utf-8')
+#     #exemplo2
+#     txBuffer = bytes([2]) + bytes([3])+ bytes("teste", 'utf-8')
     
     
-    txLen    = len(txBuffer)
-    print(txLen)
+#     txLen    = len(txBuffer)
+#     print(txLen)
 
-    # Transmite dado
-    print("tentado transmitir .... {} bytes".format(txLen))
-    com.sendData(txBuffer)
+#     # Transmite dado
+#     print("tentado transmitir .... {} bytes".format(txLen))
+#     com.sendData(txBuffer)
 
-    # espera o fim da transmissão
-    #while(com.tx.getIsBussy()):
-    #    pass
+#     # espera o fim da transmissão
+#     #while(com.tx.getIsBussy()):
+#     #    pass
     
     
-    # Atualiza dados da transmissão
-    txSize = com.tx.getStatus()
-    print ("Transmitido       {} bytes ".format(txSize))
+#     # Atualiza dados da transmissão
+#     txSize = com.tx.getStatus()
+#     print ("Transmitido       {} bytes ".format(txSize))
 
-    # Faz a recepção dos dados
-    print ("Recebendo dados .... ")
+#     # Faz a recepção dos dados
+#     print ("Recebendo dados .... ")
     
-    #repare que o tamanho da mensagem a ser lida é conhecida!     
-    rxBuffer, nRx = com.getData(txLen)
+#     #repare que o tamanho da mensagem a ser lida é conhecida!     
+#     rxBuffer, nRx = com.getData(txLen)
 
-    # log
-    print ("Lido              {} bytes ".format(nRx))
+#     # log
+#     print ("Lido              {} bytes ".format(nRx))
     
-    print (rxBuffer)
+#     print (rxBuffer)
 
     
 
-    # Encerra comunicação
-    print("-------------------------")
-    print("Comunicação encerrada")
-    print("-------------------------")
-    com.disable()
+#     # Encerra comunicação
+#     print("-------------------------")
+#     print("Comunicação encerrada")
+#     print("-------------------------")
+#     com.disable()
 
-    #so roda o main quando for executado do terminal ... se for chamado dentro de outro modulo nao roda
-if __name__ == "__main__":
-    main()
+#     #so roda o main quando for executado do terminal ... se for chamado dentro de outro modulo nao roda
+# if __name__ == "__main__":
+#     main()
--- client-server/client.py
+++ client-server/client.py
@@ -8,10 +8,62 @@
 #  Aplicação 
 ####################################################
 
-print("comecou")
-
 from enlace import *
 import time
+
+class Client(object):
+
+  def __init__(self, serialName, nomeArquivo):
+    self.serialName = serialName
+    self.nomeArquivo = nomeArquivo
+    self.com = enlace(serialName)
+
+  def comunicate(self):
+    self.com.enable()
+
+    print("-------------------------")
+    print("Comunicação inicializada")
+    print("  porta : {}".format(self.com.fisica.name))
+    print("-------------------------")
+
+    print ("gerando dados para transmissao :")
+
+    txBuffer= open(self.nomeArquivo, "rb").read()
+
+    print("TxBuffer sem converter para int...........{}".format(txBuffer))
+
+    txLen    = len(txBuffer)
+
+    imgSize = txLen.to_bytes(4, byteorder = "little")
+
+    bufferCompleto = imgSize + txBuffer
+
+    print("Buffer completo................{}".format(bufferCompleto))
+    print("imgSize...............{}".format(imgSize))
+
+    # Transmite dado
+    print("tentado transmitir .... {} bytes".format(txLen))
+    self.com.sendData(bufferCompleto)
+
+    t0 = time.time()
+
+    print ("Recebendo dados .... ")
+
+    t1 = time.time()
+
+    vel = txLen/(t1-t0)
+
+    print("\n")
+    print("- - - - - - - - - - - - - - - - - - - - - - - - - - - - - ")
+    print("Tempo de transferência........................{}".format(t1-t0))
+    print("- - - - - - - - - - - - - - - - - - - - - - - - - - - - - ")
+    print("\n")
+    s
+    # Encerra comunicação
+    print("-------------------------")
+    print("Comunicação encerrada")
+    print("-------------------------")
+    self.com.disable()
 
 
 # Serial Com Port
@@ -23,82 +75,93 @@ serialName = "/dev/cu.usbmodem141101" # Mac    (variacao de)
 # serialName = "COM11"                  # Windows(variacao de)
 print("abriu com")
 
-def main():
-    # Inicializa enlace ... variavel com possui todos os metodos e propriedades do enlace, que funciona em threading
-    com = enlace(serialName) # repare que o metodo construtor recebe um string (nome)
-    # Ativa comunicacao
-    com.enable()
+# def main():
+#     # Inicializa enlace ... variavel com possui todos os metodos e propriedades do enlace, que funciona em threading
+#     com = enlace(serialName) # repare que o metodo construtor recebe um string (nome)
+#     # Ativa comunicacao
+#     com.enable()
 
    
 
-    # Log
-    print("-------------------------")
-    print("Comunicação inicializada")
-    print("  porta : {}".format(com.fisica.name))
-    print("-------------------------")
+#     # Log
+#     print("-------------------------")
+#     print("Comunicação inicializada")
+#     print("  porta : {}".format(com.fisica.name))
+#     print("-------------------------")
 
-    # Carrega dados
-    print ("gerando dados para transmissao :")
+#     # Carrega dados
+#     print ("gerando dados para transmissao :")
   
-      #no exemplo estamos gerando uma lista de bytes ou dois bytes concatenados
+#       #no exemplo estamos gerando uma lista de bytes ou dois bytes concatenados
     
-    #exemplo 1
-    #ListTxBuffer =list()
-    #for x in range(1,10):
-    #    ListTxBuffer.append(x)
-    #txBuffer = bytes(ListTxBuffer)
+#     #exemplo 1
+#     #ListTxBuffer =list()
+#     #for x in range(1,10):
+#     #    ListTxBuffer.append(x)
+#     #txBuffer = bytes(ListTxBuffer)
     
-    #exemplo2
-    #txBuffer = bytes([2]) + bytes([3])+ bytes("teste", 'utf-8')
-    txBuffer= open("cavalo.jpeg", "rb").read()
-    #   txBuffer0 = aa.read()
-    #   f = bytearray(txBuffer0)
+#     #exemplo2
+#     #txBuffer = bytes([2]) + bytes([3])+ bytes("teste", 'utf-8')
+#     txBuffer= open("cavalo.jpeg", "rb").read()
+#     #   txBuffer0 = aa.read()
+#     #   f = bytearray(txBuffer0)
 
-    print("TxBuffer sem converter para int...........{}".format(txBuffer))
-    
-    
-    txLen    = len(txBuffer)
-    print(txLen)
-
-    imgSize = txLen.to_bytes(4, byteorder = "little")
-
-    bufferCompleto = imgSize + txBuffer
-
-    print("Buffer completo................{}".format(bufferCompleto))
-    print("imgSize...............{}".format(imgSize))
-
-    # Transmite dado
-    print("tentado transmitir .... {} bytes".format(txLen))
-    com.sendData(bufferCompleto)
-
-    # espera o fim da transmissão
-    #while(com.tx.getIsBussy()):
-    #    pass
+#     print("TxBuffer sem converter para int...........{}".format(txBuffer))
     
     
-    # Atualiza dados da transmissão
-    #txSize = com.tx.getStatus()
-    #print ("Transmitido       {} bytes ".format(txSize))
+#     txLen    = len(txBuffer)
+#     print(txLen)
 
-    # Faz a recepção dos dados
-    print ("Recebendo dados .... ")
+#     imgSize = txLen.to_bytes(4, byteorder = "little")
+
+#     bufferCompleto = imgSize + txBuffer
+
+#     print("Buffer completo................{}".format(bufferCompleto))
+#     print("imgSize...............{}".format(imgSize))
+
+#     # Transmite dado
+#     print("tentado transmitir .... {} bytes".format(txLen))
+#     com.sendData(bufferCompleto)
+
+#     t0 = time.time()
+
+#     # espera o fim da transmissão
+#     #while(com.tx.getIsBussy()):
+#     #    pass
     
-    #repare que o tamanho da mensagem a ser lida é conhecida!     
-    #rxBuffer, nRx = com.getData(txLen)
-
-    # log
-    # print ("Lido              {} bytes ".format(nRx))
     
-    # print (rxBuffer)
+#     # Atualiza dados da transmissão
+#     #txSize = com.tx.getStatus()
+#     #print ("Transmitido       {} bytes ".format(txSize))
+
+#     # Faz a recepção dos dados
+#     print ("Recebendo dados .... ")
+    
+#     #repare que o tamanho da mensagem a ser lida é conhecida!     
+#     #rxBuffer, nRx = com.getData(txLen)
+
+#     # log
+#     # print ("Lido              {} bytes ".format(nRx))
+    
+#     # print (rxBuffer)
 
     
+#     t1 = time.time()
 
-    # Encerra comunicação
-    print("-------------------------")
-    print("Comunicação encerrada")
-    print("-------------------------")
-    com.disable()
+#     vel = txLen/(t1-t0)
 
-    #so roda o main quando for executado do terminal ... se for chamado dentro de outro modulo nao roda
-if __name__ == "__main__":
-    main()
+#     print("\n")
+#     print("- - - - - - - - - - - - - - - - - - - - - - - - - - - - - ")
+#     print("Tempo de transferência........................{}".format(t1-t0))
+#     print("- - - - - - - - - - - - - - - - - - - - - - - - - - - - - ")
+#     print("\n")
+#     s
+#     # Encerra comunicação
+#     print("-------------------------")
+#     print("Comunicação encerrada")
+#     print("-------------------------")
+#     com.disable()
+
+#     #so roda o main quando for executado do terminal ... se for chamado dentro de outro modulo nao roda
+# if __name__ == "__main__":
+#     main()
