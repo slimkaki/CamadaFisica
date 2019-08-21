45b1adee2a9c56d7b0a94f34e7d78b6e628763fb
slimkaki <rafaalmada98@gmail.com>
2019-08-21 16:35:45 -0300

    server until now

--- datagrama/server.py
+++ datagrama/server.py
@@ -38,32 +38,48 @@ class Server(object):
     print ("Leitura do tamanho da imagem..................................{}  ".format(tamanhoIntimagem))
 
     rxBuffer, nRx = self.com.getData(tamanhoIntimagem)
-    rxLeitura = rxBuffer.split()'/'
+    print("\nlen do rxBuffer...........{}\n".format(len(rxBuffer)))
 
-    contador = ""
-    i = 0 # contador
-    while (i < len(rxLeitura)):
-      if rxLeitura[i]=="xf0":
-        contador="xf0"
-        i+=1
-      elif rxLeitura[i]=="xf1" and contador=="xf0":
-        contador+="/xf1"
-        i+=1
-      elif rxLeitura[i]=="xf2" and contador=="xf0/xf1":
-        contador+="/xf2"
-        i+=1
-      elif rxLeitura[i]=="xf3" and contador=="xf0/xf1/xf2":
-        contador+="/xf3"
-        i+=1
-      else:
-        contador=""
-        i+=1
-        pass
-      if contador=="xf0/xf1/xf2/xf3":
-        novaImagem = rxLeitura[:i-4]
-        break
-    
-    
+    dataStuff = b'\xf0\xf0\xf0\xf0'
+    EoP = b"\xf0\xf1\xf2\xf3"
+
+    novaImagem = rxBuffer
+
+    i = rxBuffer.find(EoP)
+    novaImagem = rxBuffer[:i]
+    print("tirei o EoP ja")
+
+    findDataStuff = novaImagem.find(dataStuff)
+    while (findDataStuff > 0):
+      print("to no while do dataStuff")
+      findDataStuff = novaImagem.find(dataStuff)
+      novaImagem = novaImagem[findDataStuff].replace(EoP)
+
+    # contadorEoP = b"" # Contador do EoP
+    # i = 0 # contador
+    # while (i < len(rxBuffer)):
+    #   print("AMIGO ESTO AQUI")
+    #   if rxBuffer[i]==b"\xf0":
+    #     contadorEoP=b"\xf0"
+    #     i+=1
+    #   elif rxBuffer[i]==b"\xf1" and contadorEoP==b"\xf0":
+    #     contadorEoP+=b"\xf1"
+    #     i+=1
+    #   elif rxBuffer[i]==b"\xf2" and contadorEoP==b"\xf0\xf1":
+    #     contadorEoP+=b"\xf2"
+    #     i+=1
+    #   elif rxBuffer[i]==b"\xf3" and contadorEoP==b"\xf0\xf1\xf2":
+    #     contadorEoP+=b"\xf3"
+    #     i+=1
+    #   else:
+    #     contadorEoP=b""
+    #     i+=1
+    #     pass
+    #   if contadorEoP==b"\xf0\xf1\xf2\xf3":
+    #     novaImagem = rxBuffer[:i-4]
+    #     break
+
+    novaImagem = novaImagem.replace(dataStuff, EoP)
 
     # Faz a recepção dos dados
     print ("Recebendo dados .... ")
