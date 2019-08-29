1226854641053bed9f82547735eb0c23f7f23c0e
slimkaki <rafaalmada98@gmail.com>
2019-08-29 14:38:15 -0300

    Iniciando projeto 4

---
+++ 04-protocoloUART/server.py
@@ -0,0 +1,115 @@
+
+#!/usr/bin/env python3
+#  -*- coding: utf-8 -*-
+#####################################################
+# Camada Física da Computação
+#Carareto
+#17/02/2018
+#  Aplicação 
+####################################################
+
+from enlace import *
+import time
+
+class Server(object):
+
+  def __init__(self, serialName, nomeArquivo):
+    self.serialName = serialName
+    self.nomeArquivo = nomeArquivo
+    self.com = enlace(serialName)
+
+  def comunicate(self):
+    self.com.rx.clearBuffer()
+    self.com.enable()
+    print("-------------------------")
+    print("Comunicação inicializada")
+    print("  porta : {}".format(self.com.fisica.name))
+    print("-------------------------")
+    print ("iniciando recebimento:")
+
+    while(self.com.rx.getIsEmpty()):
+      print("  ")
+      print("-------------------------------")
+      print("Aguardando pacote de informação")
+      pass
+
+    head, head_size = self.com.getData(4)
+    print ("\nRecebendo dados .... ")
+
+    dataStuff = b'\xf0\xf0\xf0\xf0'
+    EoP = b"\xf0\xf1\xf2\xf3"
+
+    print('\n- - - - - - - - - - - - - - - - -')
+    print('  Protocolo de Empacotamento ')
+    print('Head...............{} bytes'.format(head_size))
+    print('EoP................{}'.format(EoP))
+    print('Data Stuffing......{}'.format(dataStuff))
+    print('- - - - - - - - - - - - - - - - -')
+
+    Tpack = head[:2]
+    Tpack = int.from_bytes(Tpack,byteorder = "little")
+    print("\n===================================================")
+    print("Número total de pacotes.......................{0}".format(Tpack))
+    print("===================================================\n")
+
+
+    Npack = head[2:]
+    Npack = int.from_bytes(Npack,byteorder = "little")
+
+    Payload = b""
+    
+    boolean = False
+
+    while (Npack<Tpack):
+
+      if (boolean):
+        while(self.com.rx.getIsEmpty()):
+          pass
+        head, head_size = self.com.getData(4)
+        Npack = head[2:]
+        Npack = int.from_bytes(Npack,byteorder = "little")
+        toConclude = round((Npack/Tpack)*100,1)
+        print("Recebendo pacotes................{}%\r".format(toConclude), end='\r')
+    
+      Pacote, Pack_Size = self.com.getData(132)
+      boolean = True
+
+      i = Pacote.find(EoP)
+      if (i > 0):
+        if (len(Pacote[i:])==len(EoP)):
+          Payload += Pacote[:i].replace(dataStuff, EoP)
+          ans = 1
+          ans = ans.to_bytes(128, byteorder='little')
+          sendAnswer = head + ans + EoP 
+          self.com.sendData(sendAnswer)
+        else:
+          #print("===========================================")
+          #print("ERRO: EoP NÃO ENCONTRADO NO LOCAL ESPERADO")
+          #print("===========================================")
+          ans = 2
+          ans = ans.to_bytes(128, byteorder='little')
+          sendAnswer = head + ans + EoP 
+          self.com.sendData(sendAnswer)
+      else:
+        #print("============================")
+        #print("ERRO: EoP NÃO ENCONTRADO")
+        #print("============================")
+        ans = 0
+        ans = ans.to_bytes(128, byteorder='little')
+        sendAnswer = head + ans + EoP 
+        self.com.sendData(sendAnswer)
+
+    # Faz a recepção dos dados
+
+    open(self.nomeArquivo, "wb").write(Payload)
+
+    print("\n")
+    print("- - - - - - - - - - - - - - - - -")
+    print("Arquivo foi salvo no diretório com o nome de: {}".format(self.nomeArquivo))
+    print("- - - - - - - - - - - - - - - - -")
+    print("- - - - - - - - - - - - - - - - -")
+    print("\n")
+    print("-------------------------")
+    print(" Comunicação encerrada  ")
+    print("-------------------------")
+    self.com.disable()
